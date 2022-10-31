0d8346518b7a3588c89f2b587f14b1a84d78a695
yang-zebo <1282258957@qq.com>
2022-10-31 16:16:30 +0800

    yzb

---
+++ src/components/BaseDialog/index.vue
@@ -0,0 +1,71 @@
+<script setup name="BaseDialog">
+import { ref } from 'vue'
+const { btnText, btnType, btnSize, dialogWidth, title, cancelText, confirmText } = defineProps({
+  btnText: {
+    type: String,
+    default: 'Button'
+  },
+  btnType: {
+    type: String,
+    default: 'primary'
+  },
+  btnSize: {
+    type: String,
+    default: 'default'
+  },
+  dialogWidth: {
+    type: String,
+    default: '50%'
+  },
+  title: {
+    type: String,
+    default: 'Tips'
+  },
+  cancelText: {
+    type: String,
+    default: 'Cancel'
+  },
+  confirmText: {
+    type: String,
+    default: 'Confirm'
+  }
+})
+
+const dialogVisible = ref(false)
+
+function openDialog() {
+  dialogVisible.value = true
+}
+function closeDialog() {
+  dialogVisible.value = false
+}
+function confirmHandle() {
+  dialogVisible.value = false
+}
+</script>
+
+<template>
+  <slot name="button" :type="btnType" :size="btnSize" :btnText="btnText" :openDialog="openDialog">
+    <el-button :type="btnType" :size="btnSize" @click="openDialog">{{ btnText }}</el-button>
+  </slot>
+  <el-dialog v-model="dialogVisible" :title="title" :width="dialogWidth" draggable>
+    <template #header="{close, titleId, titleClass}">
+      <slot name="header" :close="close" :titleId="titleId" :titleClass="titleClass"></slot>
+    </template>
+    <slot>
+      Content
+    </slot>
+    <template #footer>
+      <slot name="footer" :closeDialog="closeDialog" :dialogVisible="dialogVisible">
+        <el-button @click="closeDialog">{{ cancelText }}</el-button>
+        <el-button type="primary" @click="confirmHandle">
+          {{ confirmText }}
+        </el-button>
+      </slot>
+    </template>
+  </el-dialog>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/test/test1/index.vue
+++ src/views/test/test1/index.vue
@@ -1,8 +1,8 @@
 <script setup>
 import BaseForm from '@/components/BaseForm/index.vue'
+import BaseDialog from '@/components/BaseDialog/index.vue'
 import { option } from '@/components/BaseForm/option.js'
 import { reactive } from 'vue'
-
 const form = reactive({})
 
 </script>
@@ -10,6 +10,8 @@ const form = reactive({})
 <template>
   <div class="card-container">
     <base-form :option="option" :form="form"></base-form>
+    <BaseDialog>
+    </BaseDialog>
   </div>
 </template>
 
